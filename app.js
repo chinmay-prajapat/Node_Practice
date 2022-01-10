01b54289d6a75829de8dc63e6c5f7900b03a08fa
chinmay-prajapat <prajapatchinmay@gmail.com>
2022-01-10 21:23:54 +0530

    readFile and writeFile

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-const { readFileSync, writeFileSync } = require("fs");
-const first = readFileSync("./content/first.txt", "utf-8");
-const second = readFileSync("./content/second.txt", "utf-8");
-writeFileSync(
-  "./content/result-sync.txt",
-  `Here is the result: ${first},${second}`,
-  { flag: "a" }
-);
+const { readFile, writeFile } = require("fs");
+
+readFile("./content/first.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf-8", (err, res) => {
+    if (err) {
+      console.log(err);
+      return err;
+    } else {
+      const second = res;
+      writeFile(
+        "./content/result-async.txt",
+
+        `Here is the result: ${first},${second}`,
+        { flag: "a" },
+        (err, res) => {
+          if (err) {
+            console.log(err);
+            return err;
+          } else {
+            console.log(res);
+          }
+        }
+      );
+    }
+  });
+});
