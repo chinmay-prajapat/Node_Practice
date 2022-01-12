cccd75cbc970b27178856fdd5fd72db801c53ffb
chinmay-prajapat <prajapatchinmay@gmail.com>
2022-01-12 16:39:13 +0530

    events=>EventEmitter

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-const { readFile } = require("fs");
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
-
-getText("./content/first.txt")
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => console.log(err.message));
+const EventEmitter = require("events");
+const customEmitter = new EventEmitter();
+customEmitter.on("response", () => {
+  console.log(`data received`);
+});
+customEmitter.emit("response");
