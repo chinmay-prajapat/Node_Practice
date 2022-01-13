3fe4fbcafa19d99a85649e9a361acd4c1e46225b
chinmay-prajapat <prajapatchinmay@gmail.com>
2022-01-13 22:00:54 +0530

    express setup

--- 2-express/app.js
+++ 2-express/app.js
@@ -1,13 +1,8 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./index.html"));
-});
-app.all("*", (req, res) => {
-  res.status(404).send("Resource Not Found!");
+  res.send("<h1>Hello</h1>");
 });
 app.listen(5000, () => {
-  console.log("Server running at 5000");
+  console.log("Server at 5000");
 });
-3;
