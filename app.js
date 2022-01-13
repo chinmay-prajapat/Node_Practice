ea47c9fb9994fe1ac6fd0963e340297e64742d25
chinmay-prajapat <prajapatchinmay@gmail.com>
2022-01-13 14:02:25 +0530

    routing traditional way

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const { createReadStream } = require("fs");
-const stream = createReadStream("./content/big.txt", { highWaterMark: 90000 });
-stream.on("data", (result) => {
-  console.log(result);
+const http = require("http");
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1>home page</h1>");
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1>about page</h1>");
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end("<h1>page not found!</h1>");
+  }
 });
-stream.on("error", (err) => console.log(err));
+server.listen(5000);
