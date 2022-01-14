ef8ca29e2eda8e8fe3f7c9aac0ef38680990d468
chinmay-prajapat <prajapatchinmay@gmail.com>
2022-01-14 20:11:27 +0530

    setup products

--- 2-express/app.js
+++ 2-express/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const { products } = require("./data");
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.send('<h1>Home Page Page</h1><a href="./data">Products</a>');
+});
+app.get("/data", (req, res) => {
+  console.log(req);
+  const newProduct = products.map((item) => {
+    const { id, name, image } = item;
+    return { id, name, image };
+  });
+  res.send(newProduct);
 });
 app.listen(5000, () => {
   console.log("Server at 5000");
